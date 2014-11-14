88077a2cb164201459acc414dda5482978cafd22
TheTime <Edvard.Thornros@gmail.com>
2014-11-14 14:37:09 +0100

    Implemented the core of the physics engine

--- src/sk/engine/physics/PhysicsEngine.java
+++ src/sk/engine/physics/PhysicsEngine.java
@@ -1,14 +1,42 @@
 package sk.engine.physics;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 import sk.engine.physics.collision.Collider;
 
 public class PhysicsEngine {
 	
-	private ArrayList<Collider> colliders;
+	private ArrayList<RigidBody> bodies;
+	private Stack<RigidBody> deathPool;
+	
 	
 	public PhysicsEngine() {
-		colliders = new ArrayList<>();
+		bodies = new ArrayList<>();
 	}
+	
+	public void update(double tick) {
+		//Send dead rigid bodies to death pool
+		for(RigidBody rb : deathPool)
+			if(bodies.contains(rb))
+				bodies.remove(rb);
+		
+		//Update the bodies if needed
+		for(RigidBody rb : bodies) {
+			if(rb.isAlive()) {
+				if(rb.hasMagnitude())
+					rb.update();
+			} else {
+				deathPool.add(rb);
+			}
+		}
+	}
+	
+	public PhysicsEngine addRigidBody(RigidBody... bodies) {
+		for(RigidBody rb : bodies)
+			this.bodies.add(rb);
+		
+		return this;
+	}
+	
 }
