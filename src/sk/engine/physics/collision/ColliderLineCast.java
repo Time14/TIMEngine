5a9e205dbda123bd1a0a4ade85a28805117972ed
TheTime <Edvard.Thornros@gmail.com>
2014-11-15 17:44:03 +0100

    Added some bits of code.

--- src/sk/engine/physics/collision/ColliderLineCast.java
+++ src/sk/engine/physics/collision/ColliderLineCast.java
@@ -1,5 +1,6 @@
 package sk.engine.physics.collision;
 
+import sk.engine.vector.Vector2f;
 import sk.engine.vector.Vector4f;
 
 public class ColliderLineCast extends Collider {
@@ -16,5 +17,25 @@ public class ColliderLineCast extends Collider {
 		
 		return false;
 	}
+	 /*
+	  * NP Sporre
+	  */
+	public double line(Vector2f point1, Vector2f point2, Vector2f pointToTest) {
+		double deltaX = point1.x - point2.x;
+		double deltaY = point1.y - point2.y;
+		double m = point1.y - (deltaY/deltaX * point1.x);
+		if (deltaY == 0 && point1.x < pointToTest.x ) 
+			return 1;
+		else if (deltaX == 0 && pointToTest.y == point1.y) 
+			return 1;
+		else if ( (pointToTest.y - m)/( deltaY/deltaX) < pointToTest.x ) {
+			return 1;
+		}
+		else
+			return 0;
+	}
 	
+	public boolean isOdd(int n){
+		return n % 2 == 1;
+	}	
 }
